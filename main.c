608f47f176c563576516b0b8c6a7dbea0b03fdee
David <david.04.fsl@gmail.com>
2023-08-29 16:23:19 +0200

    added check for duplicates

--- main.c
+++ main.c
@@ -6,11 +6,34 @@
 /*   By: dlima <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 17:29:13 by dlima             #+#    #+#             */
-/*   Updated: 2023/08/29 11:07:31 by dlima            ###   ########.fr       */
+/*   Updated: 2023/08/29 16:14:54 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_duplicates(t_stack *x)
+{
+	int	i;
+	int	j;
+	int	cur;
+
+	i = 0;
+	while (i < x->size)
+	{
+		cur = x->stack[i];
+		j = 0;
+		while (j < x->size)
+		{
+
+			if (cur == x->stack[j] && i != j)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
 void	get_list(int *stack_a, char *argv[], int size)
 {
@@ -72,7 +95,15 @@ int	main(int argc, char *argv[])
 	b->size = 0;
 	b->stack = (int *)ft_calloc(a->size, sizeof(int));
 	get_list(a->stack, argv, a->size);
-
+	if (!(check_duplicates(a)))
+	{
+		free(a->stack);
+		free(b->stack);
+		free(a);
+		free(b);
+		ft_printf("ERROR\n");
+		return (0);
+	}
 
 	print_stack(a, b);
 	write(1, "\n", 1);
@@ -85,3 +116,4 @@ int	main(int argc, char *argv[])
 
 //check duplicates
 //check for max int and min int
+//should I check for 0 and -0?
