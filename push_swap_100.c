6917b39de13bcba1c8efdb9fe5eca8cbc0428e85
David <david.04.fsl@gmail.com>
2023-08-30 18:38:45 +0200

    fixed small error on pivot function

--- push_swap_100.c
+++ push_swap_100.c
@@ -6,7 +6,7 @@
 /*   By: dlima <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 15:38:14 by dlima             #+#    #+#             */
-/*   Updated: 2023/08/30 18:21:31 by dlima            ###   ########.fr       */
+/*   Updated: 2023/08/30 18:37:22 by dlima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	get_pivots(t_stack *a_cpy, int n_chunks)
 	a_cpy->pivots = malloc(sizeof(int) * n_chunks);
 	while (i < n_chunks - 1)
 	{
-		a_cpy->pivots[i] = a_cpy->stack[chunk_size * (i + 1)];
+		a_cpy->pivots[i] = a_cpy->stack[chunk_size * (i + 1) - 1];
 		i++;
 	}
 	a_cpy->pivots[n_chunks - 1] = a_cpy->stack[a_cpy->size - 1];
@@ -42,13 +42,12 @@ void	push_swap_100(t_stack *a, t_stack *b)
 	a->pivots = a_cpy->pivots;
 	free(a_cpy->stack);
 	free(a_cpy);
-	// int i = 0;
-	// write(1, "\n",1);
-	// while (i < 5)
-	// {
-	// 	ft_printf("%d\n", a->pivots[i]);
-	// 	i++;
-	// }
+	int i = 0;
+	write(1, "\n",1);
+	while (i < 5)
+	{
+		ft_printf("%d\n", a->pivots[i]);
+		i++;
+	}
 	// print_stack(a_cpy, a);
-
 }
